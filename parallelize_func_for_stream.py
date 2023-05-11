09999487f676f7c709d211d6a95ed901e8a600a2
Frederik Beimgraben <46428177+frederikbeimgraben@users.noreply.github.com>
2023-05-11 20:32:30 +0200

    Update parallelize_func_for_stream.py

--- parallelize_func_for_stream.py
+++ parallelize_func_for_stream.py
@@ -5,7 +5,7 @@ It can be fed with a continuous stream of data and will also yield one.
 Usage:
     @mappable(
         max_workers = W,            # Use <W: int> workers
-        break_on_except = (..., ),  # Break on exceptions of type <E> (tuple)
+        break_on_except = (..., ),  # Break on exceptions of types ... (tuple)
                                     # -> by default all break the stream
         timed = True | False,       # Return the time it took to run a worker in the output stream
         ordered = True | False,     # Return the results in the order they were called
@@ -29,15 +29,17 @@ import time
 import dill
 
 
-# A function that unpacks the pickled function and runs it
-def _run(
+def __run(
         function_pickled: bytes,
         queue: multiprocessing.Queue,
         worker_id: int,
         *args,
         **kwargs) -> Tuple[float, Any]:
     """
-    Unpacks a pickled function and runs it.
+    Unpacks a pickled function, runs it and puts the result into the queue
+    This is necessary because multiprocessing can't pickle all functions and thus we have to pickle
+    them manually using dill.
+    The function is on module level, so it can be passed to the workers.
     """
     # Unpickle the function
     function = dill.loads(function_pickled)
@@ -55,97 +57,115 @@ def _run(
         queue.put((worker_id, end - start, exc,))
 
 
-def mappable(
+def async_map(
         max_workers: int=None,
         break_on_except: Iterable[Exception]=(Exception,),
         timed: bool=False,
-        ordered: bool=False) -> Callable:
+        ordered: bool=False,
+        timeout: int=5,
+        wait: bool=True) -> Callable:
     """
     Decorator to automatically parallelize a function like:
         foo(A) -> B: ...
 
-    =>  foo(Iterable[A]) -> Iterable[Tuple[{time}, B]]: ...
+    =>  foo(Iterable[A]) -> Generator[Tuple[{time}, B]]: ...
 
     This way it can handle continuous streams of data at a higher performance.
     """
 
-    def eval_result(
-            workers: List[multiprocessing.Process],
-            queue: multiprocessing.Queue) -> Any:
+    
+
+    def wrapper(function: Callable) -> Callable:
         """
-        Evaluate the result of a process.
+        The Function Wrapper
         """
-
-        elem: Any
-        found: bool = False
-
-        if ordered: # Pick the next worker
-            worker = workers.pop(0)
-            worker[1].join()
-            elem = queue.get()
-
-            while not found:
-                for _ in range(queue.qsize() + 1):
-                    if elem[0] == worker[0]:
-                        found = True
-                        break
-                    else:
-                        queue.put(elem)
-                        elem = queue.get()
         
-        else: # Pick any worker, that is finished
-            # Wait for a worker to finish
-            while not found:
-                for worker_id, worker in workers:
-                    if not worker.is_alive():
-                        worker.join()
-                        workers.remove((worker_id, worker,))
-                        found = True
-                        break
-            elem = queue.get()
+        def eval_result(
+            workers: List[Tuple[int, multiprocessing.Process]],
+            queue: multiprocessing.Queue) -> Any:
+            """
+            Evaluate the result of a process.
+            """
 
-        # Match the result
-        match elem:
-            # Make the linter ignore used before assignment
-            # pylint: disable=used-before-assignment
-            case (_, _, result,) if any(
-                isinstance(result, exception)
-                for exception in break_on_except):
-                # If the result is an exception, raise it
-                for _, worker in workers:
-                    worker.terminate()
-                raise result
-            case (_, took, result,):
-                # If the result is not an exception, return it
-                return (took, result,) if timed else result
+            # Element to be processed
+            elem: Any = None
 
-    def _wrapper(function: Callable) -> Callable:
+            # Has it been found?
+            found: bool = False
+
+            try:
+                if ordered: # Pick the next worker in order
+                    worker_id, worker = workers.pop(0)
+                    worker.join(timeout=timeout)
+                    
+                    elem = queue.get()
+                    while not found:
+                        if elem[0] == worker_id:
+                            raise StopIteration()
+                        else:
+                            # Cycle through the results
+                            queue.put(elem)
+                            elem = queue.get()
+                
+                else: # Pick any worker, that is finished
+                    start = time.time()
+                    while not found and time.time() - start < timeout:
+                        for worker_id, worker in workers:
+                            if not worker.is_alive():
+                                worker.join(timeout=timeout - (time.time() - start))
+                                workers.remove((worker_id, worker,))
+                                elem = queue.get()
+                                raise StopIteration()
+                    
+                    # If it timed out, stop a worker.
+                    worker_id, worker = workers.pop(0)
+                    worker.join(timeout=0)
+                    elem = queue.get()
+            except StopIteration:
+                pass
+
+            # Match the result
+            match elem:
+                # Make the linter ignore used before assignment
+                # pylint: disable=used-before-assignment
+                case (_, _, result,) if any(
+                    isinstance(result, exception)
+                    for exception in break_on_except):
+                    # If the result is an exception, raise it
+                    for _, worker in workers:
+                        worker.terminate()
+                    raise result
+                case (_, took, result,):
+                    # If the result is not an exception, return it
+                    return (took, result,) if timed else result
+                case None:
+                    return (-1, None,)
+
         # Get the signature of the function
         signature = inspect.signature(function)
 
         # Get the parameter and return annotations
-        param_annotation = Tuple[tuple(
+        params = Tuple[tuple(
             Optional[parameter.annotation]
             for parameter in signature.parameters.values()
         )]
-        return_annotation = signature.return_annotation
+        returns = signature.return_annotation
 
         # This is the `replacement` function that will be returned
-        def _new(
-            input: Iterable[param_annotation] | param_annotation,
+        def new(
+            input_stream: Iterable[params],
             *global_args,
-            **global_kwargs) -> Iterable[return_annotation] | return_annotation:
-            # If the input is not an iterable, just run the function
-            if (
-                not isinstance(input, Iterable)):
-                return function(input, *global_args, **global_kwargs)
+            **global_kwargs) -> Iterable[returns]:
 
-            # If the input stream is not a generator of tuples, make it one
-            input_stream = (
-                (arg,) if not isinstance(arg, tuple) else arg
-                for arg in input
-            )
-
+            # Check if the input stream is iterable
+            if not '__next__' in dir(input_stream):
+                if '__iter__' in dir(input_stream):
+                    input_stream = iter(input_stream)
+                else:
+                    raise TypeError(
+                        'The input stream must implement the `__next__` or `__iter__` method'
+                    )
+                
             # Pickle the function
             _function = dill.dumps(function)
 
@@ -158,44 +178,63 @@ def mappable(
             # Create a queue to communicate with the workers
             queue = multiprocessing.Queue()
 
-            # manually create a pool using multiprocessing.Process
-            # because the multiprocessing.Pool class does not support
-            # returning continuously from a generator
-            for args in input_stream:
-                # Create a worker and start it
-                worker = multiprocessing.Process(
-                    target=_run,
-                    args=(_function, queue, worker_id, *args, *global_args),
-                    kwargs=global_kwargs
-                )
-                worker.start()
-                workers.append((worker_id, worker,))
+            # Equivalent to a Pool
+            # FIXME: Will be reworked to reuse workers in the future
+            while True:
+                try:
+                    while True:
+                        # Get the next arguments
+                        # Will raise StopIteration if the stream is empty
+                        # This will be caught by the outer try-except
+                        # If we would use a for loop, it would catch the exception 
+                        # which we don't want
+                        args = next(input_stream)
 
-                # Increment the id
-                worker_id += 1
+                        # If the arguments are not a tuple, make them a tuple
+                        args = (args, ) if not isinstance(args, tuple) else args
 
-                # If our pool is full, yield a result
-                while (
-                    len(workers) >= (max_workers or multiprocessing.cpu_count())
-                ):
-                    yield eval_result(
-                        workers,
-                        queue
-                    )
+                        # Create a worker and start it
+                        worker = multiprocessing.Process(
+                            target=__run,
+                            args=(_function, queue, worker_id, *args, *global_args),
+                            kwargs=global_kwargs
+                        )
+                        worker.start()
 
-            # If there are still workers, yield the results
-            while workers:
-                yield eval_result(
-                    workers,
-                    queue
-                )
+                        # Add the worker to the list
+                        workers.append((worker_id, worker,))
+
+                        # Increment the id
+                        worker_id += 1
+
+                        # If our pool is full, yield a result
+                        while (
+                            len(workers) >= (max_workers or multiprocessing.cpu_count())
+                        ):
+                            yield eval_result(
+                                workers,
+                                queue
+                            )
+                
+                # If the stream is empty, handle like:
+                except StopIteration:
+                    # If `wait` is not set, break
+                    if queue.empty() and not wait:
+                        break
+                    # Otherwise yield the results and wait for new ones
+                    if not queue.empty():
+                        yield eval_result(
+                            workers,
+                            queue
+                        )
+                        continue
 
         # Overwrite the docstring and name of the function
-        _new.__doc__ = function.__doc__
-        _new.__name__ = function.__name__
+        new.__doc__ = function.__doc__
+        new.__name__ = function.__name__
 
         # Return the new function
-        return _new
+        return new
 
     # Return the wrapper
-    return _wrapper
+    return wrapper
